12836f37da7b6eabc2f150c6b6a7c58c0a9c716d
Daniel <da222xg@student.lnu.se>
2022-09-16 23:27:12 +0200

    Add and set animation loops.

--- src/Sprite.js
+++ src/Sprite.js
@@ -16,21 +16,77 @@ export class Sprite {
     this.y = 0
     this.width = 300
     this.height = 200
-    
-    
-    this.image = new Image()
-    this.image.src = './png/Dead (1).png'
 
-    this.image.addEventListener('load', () => {
-      this.draw()
-    })
+    this.animations = {}
+    this.currentAnimation = {}
 
+    this.currentFrame = {}
+    this.currentFrameIndex = 0
   }
 
   /**
-   * Draws frame to canvas.
+   * Initiates animation loop.
    */
-  draw() {
-    this.context.drawImage(this.image, this.x, this.y, this.width, this.height)
+  update() {
+    if (
+      this.currentFrameIndex >=
+      (this.currentAnimation.images.length || this.currentAnimation.images.frameCount)
+    ) {
+      this.currentFrameIndex = 0
+    }
+
+    this.currentFrame = this.currentAnimation.images[this.currentFrameIndex]
+    this.currentFrameIndex++
+
+    this.#draw()
+    this.startTime = this.currentTime
+  }
+
+  /**
+   * Draws current frame to canvas.
+   */
+  #draw() {
+    this.context.clearRect(0, 0, innerWidth, innerHeight)
+    this.context.drawImage(this.currentFrame, this.x, this.y, this.width, this.height)
+  }
+
+  /**
+   * Adds animation loop to sprite instance.
+   *
+   * @param {Object} options - { name: String, image: { src: [String] } }.
+   */
+  addAnimation(options) {
+    const { name, images } = options
+
+    if (this.animations[name]) {
+      throw new Error(`Sprite ${name}: animation already exists.`)
+    }
+
+    let frames = []
+
+    for (let i = 0; i < images.src.length; i++) {
+      const image = new Image()
+      image.src = images.src[i]
+
+      frames.push(image)
+    }
+
+    this.animations[name] = {
+      name: name,
+      images: frames
+    }
+  }
+
+  /**
+   * Sets current animation loop.
+   *
+   * @param {String} name - Name attribute of animation object.
+   */
+  setCurrentAnimation(name) {
+    if (this.animations[name]) {
+      this.currentAnimation = this.animations[name]
+    } else {
+      throw new Error(`Sprite '${name}': animation ${name} is not defined.`)
+    }
   }
 }
--- test.js
+++ test.js
@@ -5,3 +5,27 @@ canvas.width = innerWidth
 canvas.height = innerHeight
 
 const player = new Sprite('player', canvas)
+
+player.addAnimation({
+  name: 'Walk',
+  images: {
+    src: [
+      './png/Run (1).png',
+      './png/Run (2).png',
+      './png/Run (3).png',
+      './png/Run (4).png',
+      './png/Run (5).png',
+      './png/Run (6).png',
+      './png/Run (7).png',
+      './png/Run (8).png'
+    ]
+  }
+})
+
+player.setCurrentAnimation('Walk')
+
+function animate() {
+  requestAnimationFrame(animate)
+  player.update()
+}
+animate()
